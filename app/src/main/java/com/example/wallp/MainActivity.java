c1e3d2d4120a87447731874ef924a97f204ea4ff
kumarichhavi <kumari.chhavi_cs17@gla.ac.in>
2019-10-19 15:12:18 +0530

    MidTerm Report (Wallpaper Management)

---
+++ app/src/main/java/com/example/wallp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.wallp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2,b3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.t1);
+        b2=findViewById(R.id.t2);
+        b3=findViewById(R.id.t3);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Landscape.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/wallp/Splash.java
@@ -0,0 +1,26 @@
+package com.example.wallp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView = findViewById(R.id.spl);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
