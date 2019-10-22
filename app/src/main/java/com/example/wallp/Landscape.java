c400bb729a394969ff621838d58c947b5b406605
vishalcs17 <vishal.rawal_cs17@gla.ac.in>
2019-10-22 11:17:19 +0530

    Grid View added and a new ImageAdapter class is updated.

--- app/src/main/java/com/example/wallp/Landscape.java
+++ app/src/main/java/com/example/wallp/Landscape.java
@@ -1,7 +1,11 @@
 package com.example.wallp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class Landscape extends AppCompatActivity {
@@ -11,8 +15,17 @@ public class Landscape extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landscape);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
+        GridView gridview = findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(),Singleview.class);
+                intent.putExtra("id",i);
+                startActivity(intent);
+            }
+        });
 
 
 
