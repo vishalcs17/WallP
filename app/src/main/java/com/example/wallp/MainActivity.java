cad7a6731b498b311764f90d9859fbc683aa0949
vishalcs17 <vishal.rawal_cs17@gla.ac.in>
2019-11-09 12:11:50 +0530

    Gallery Module Added

--- app/src/main/java/com/example/wallp/MainActivity.java
+++ app/src/main/java/com/example/wallp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.wallp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.app.WallpaperManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1,b2,b3,b4,exit;
-    TextView textView;
+    Button b1,b2,b3,b4,b5,exit;
+    TextView textView,textTargetUri;
 
 
     @Override
@@ -21,8 +30,10 @@ public class MainActivity extends AppCompatActivity {
         b2=findViewById(R.id.t2);
         b3=findViewById(R.id.t3);
         b4=findViewById(R.id.t4);
+        b5=findViewById(R.id.t5);
         exit=findViewById(R.id.exit);
         textView=findViewById(R.id.detail);
+        textTargetUri=findViewById(R.id.targeturi);
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +76,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,0);
+
+            }
+        });
+
         exit.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("NewApi")
             @Override
@@ -72,5 +92,28 @@ public class MainActivity extends AppCompatActivity {
                 finishAffinity();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            textTargetUri.setText(targetUri.toString());
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+                try{
+                    wallpaperManager.setBitmap(bitmap);
+                    Toast.makeText(MainActivity.this,"Wallpaper Updated",Toast.LENGTH_LONG).show();
+                }catch (IOException e2){
+                    e2.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
